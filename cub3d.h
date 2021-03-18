0545d4b0dbaf3807ef341a3e5bd1cecad6e2cd77
melperri <melperri@student.42.fr>
2021-03-18 16:45:32 +0100

    testing

--- cub3d.h
+++ cub3d.h
@@ -1,8 +1,32 @@
 #ifndef CUB3D_H
 # define CUB3D_H
+# define WINDOW_WIDTH 500
+# define WINDOW_HEIGHT 500
 # define ERROR_ARG -2 
+# define MLX_ERROR 1
 # define STR_ERROR_ARG "Error\nBad number of arguments.\n"
+# define RED_PIXEL 0xFF0000
+# define GREEN_PIXEL 0xFF00
+# define WHITE_PIXEL 0x0000
 
 # include "libft/libft.h"
+# include <mlx.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+
+typedef struct	s_data
+{
+		void	*mlx_ptr;
+		void	*win_ptr;
+}				t_data;
+
+typedef struct	s_rect
+{
+  int	x;
+  int	y;
+  int	width;
+  int	height;
+  int	color;
+}				t_rect;
 
 #endif
--- testlib.c
+++ testlib.c
@@ -1,27 +1,96 @@
-#include <stdlib.h>
-#include <mlx.h>
+#include "cub3d.h"
 
-int	main()
+int	handle_no_event(void *data)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		color;
+	(void)data;
+	return (0);
+}
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Cub3D");
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int	handle_keyrelease(int keysym, void *data)
+{
+	(void)data;
+	printf("Keyrelease: %d\n", keysym);
+	return (0);
+}
+
+/*
+int	encode_rgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+	return (red << 16 | green << 8 | blue);
+}
+*/
+
+int	render_rect(t_data *data, t_rect rect)
+{
 	int	i;
+	int	j;
 
-	i = 0;
-	while (i < 500)
+	if (data->win_ptr == NULL)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, i, 30, 200);
-		i++;
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		++i;
 	}
-	mlx_string_put(mlx_ptr, win_ptr, 25, 25, 255, "HELLO");
+	return (0);
+}
+
+void	render_background(t_data *data, int color)
+{
+	int	i;
+	int	j;
+
+	if (data->win_ptr == NULL)
+		return ;
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+		++i;
+	}
+}
+
+int	render(t_data *data)
+{
+	render_background(data, WHITE_PIXEL);
+	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3D");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	mlx_string_put(data.mlx_ptr, data.win_ptr, 25, 25, 255, "HELLO");
 
 //	mlx_new_image();
-	mlx_loop(mlx_ptr);
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+//	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
+	return (0);
 }
